45893454f0aaf030603599718ab49deec4a45515
abdulmajeed773 <46180607+abdulmajeed773@users.noreply.github.com>
2025-07-19 02:42:30 +0300

    mikrotik_push.py

--- backend /mikrotik_push.py
+++ backend /mikrotik_push.py
@@ -1,1 +1,14 @@
+from routeros_api import RouterOsApiPool
+from config import MIKROTIK_HOST, MIKROTIK_USER, MIKROTIK_PASS
 
+api_pool = RouterOsApiPool(
+    host=MIKROTIK_HOST,
+    username=MIKROTIK_USER,
+    password=MIKROTIK_PASS,
+    plaintext_login=True
+)
+api = api_pool.get_api()
+
+active = api.get_resource('/ip/hotspot/active').get()
+for user in active:
+    print(f"💻 {user['mac-address']} - {user['address']}")
